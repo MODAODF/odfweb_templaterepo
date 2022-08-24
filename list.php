e5206117fb78c1566758b1d3a53a46bea4af8270
sharon <sharon.wu@ossii.com.tw>
2022-08-24 14:36:40 +0800

    Drop \OCP\Util::isIe

--- list.php
+++ list.php
@@ -26,7 +26,6 @@ $config = \OC::$server->getConfig();
 $userSession = \OC::$server->getUserSession();
 
 $showgridview = $config->getUserValue($userSession->getUser()->getUID(), 'files', 'show_grid', false);
-$isIE = \OCP\Util::isIE();
 
 // renders the controls and table headers template
 $tmpl = new OCP\Template('templaterepo', 'list', '');
@@ -38,5 +37,5 @@ $tmpl = new OCP\Template('templaterepo', 'list', '');
 OC_Util::addStyle('templaterepo', "merged", true);
 
 // gridview not available for ie
-$tmpl->assign('showgridview', $showgridview && !$isIE);
+$tmpl->assign('showgridview', $showgridview);
 $tmpl->printPage();
