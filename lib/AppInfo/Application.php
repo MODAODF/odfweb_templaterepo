nextcloud/groupfolders

allow enabling encryption for groupfolders
Signed-off-by: Robin Appelman <robin@icewind.nl>

lib/AppInfo/Application.php
@@ -78,6 +78,7 @@ class Application extends App implements IBootstrap {
 			};
 			$config = $c->get(IConfig::class);
 			$allowRootShare = $config->getAppValue('groupfolders', 'allow_root_share', 'true') === 'true';
+			$enableEncryption = $config->getAppValue('groupfolders', 'enable_encryption', 'false') === 'true';
 
 			return new MountProvider(
 				$c->getServer()->getGroupManager(),
@@ -90,7 +91,8 @@ class Application extends App implements IBootstrap {
 				$c->get(IMountProviderCollection::class),
 				$c->get(IDBConnection::class),
 				$c->get(ICacheFactory::class)->createLocal("groupfolders"),
-				$allowRootShare
+				$allowRootShare,
+				$enableEncryption
 			);
 		});
 

lib/Mount/MountProvider.php
@@ -71,6 +71,7 @@ class MountProvider implements IMountProvider {
 	private ICache $cache;
 	private ?int $rootStorageId = null;
 	private bool $allowRootShare;
+	private bool $enableEncryption;
 
 	public function __construct(
 		IGroupManager $groupProvider,
@@ -83,7 +84,8 @@ class MountProvider implements IMountProvider {
 		IMountProviderCollection $mountProviderCollection,
 		IDBConnection $connection,
 		ICache $cache,
-		bool $allowRootShare
+		bool $allowRootShare,
+		bool $enableEncryption
 	) {
 		$this->groupProvider = $groupProvider;
 		$this->folderManager = $folderManager;
@@ -96,6 +98,7 @@ class MountProvider implements IMountProvider {
 		$this->connection = $connection;
 		$this->cache = $cache;
 		$this->allowRootShare = $allowRootShare;
+		$this->enableEncryption = $enableEncryption;
 	}
 
 	private function getRootStorageId(): int {
@@ -208,14 +211,25 @@ class MountProvider implements IMountProvider {
 			'storage' => $storage,
 			'root' => $rootPath
 		]);
-		$quotaStorage = new GroupFolderStorage([
-			'storage' => $baseStorage,
-			'quota' => $quota,
-			'folder_id' => $id,
-			'rootCacheEntry' => $cacheEntry,
-			'userSession' => $this->userSession,
-			'mountOwner' => $user,
-		]);
+		if ($this->enableEncryption) {
+			$quotaStorage = new GroupFolderStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		} else {
+			$quotaStorage = new GroupFolderNoEncryptionStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		}
 		$maskedStore = new PermissionsMask([
 			'storage' => $quotaStorage,
 			'mask' => $permissions

MODAODF/odfweb_templaterepo

lib/AppInfo/Application.php
@@ -89,6 +89,7 @@ class Application extends App implements IBootstrap {
 			};
 			$config = $c->get(IConfig::class);
 			$allowRootShare = $config->getAppValue('templaterepo', 'allow_root_share', 'true') === 'true';
+			$enableEncryption = $config->getAppValue('templaterepo', 'enable_encryption', 'false') === 'true';
 
 			return new MountProvider(
 				$c->getServer()->getGroupManager(),
@@ -101,7 +102,8 @@ class Application extends App implements IBootstrap {
 				$c->get(IMountProviderCollection::class),
 				$c->get(IDBConnection::class),
 				$c->get(ICacheFactory::class)->createLocal("templaterepo"),
-				$allowRootShare
+				$allowRootShare,
+				$enableEncryption
 			);
 		});
 

lib/Mount/MountProvider.php
@@ -71,6 +71,7 @@ class MountProvider implements IMountProvider {
 	private ICache $cache;
 	private ?int $rootStorageId = null;
 	private bool $allowRootShare;
+	private bool $enableEncryption;
 
 	public function __construct(
 		IGroupManager $groupProvider,
@@ -83,7 +84,8 @@ class MountProvider implements IMountProvider {
 		IMountProviderCollection $mountProviderCollection,
 		IDBConnection $connection,
 		ICache $cache,
-		bool $allowRootShare
+		bool $allowRootShare,
+		bool $enableEncryption
 	) {
 		$this->groupProvider = $groupProvider;
 		$this->folderManager = $folderManager;
@@ -96,6 +98,7 @@ class MountProvider implements IMountProvider {
 		$this->connection = $connection;
 		$this->cache = $cache;
 		$this->allowRootShare = $allowRootShare;
+		$this->enableEncryption = $enableEncryption;
 	}
 
 	private function getRootStorageId(): int {
@@ -208,14 +211,25 @@ class MountProvider implements IMountProvider {
 			'storage' => $storage,
 			'root' => $rootPath
 		]);
-		$quotaStorage = new TemplateRepoStorage([
-			'storage' => $baseStorage,
-			'quota' => $quota,
-			'folder_id' => $id,
-			'rootCacheEntry' => $cacheEntry,
-			'userSession' => $this->userSession,
-			'mountOwner' => $user,
-		]);
+		if ($this->enableEncryption) {
+			$quotaStorage = new TemplateRepoStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		} else {
+			$quotaStorage = new TemplateRepoNoEncryptionStorage([
+				'storage' => $baseStorage,
+				'quota' => $quota,
+				'folder_id' => $id,
+				'rootCacheEntry' => $cacheEntry,
+				'userSession' => $this->userSession,
+				'mountOwner' => $user,
+			]);
+		}
 		$maskedStore = new PermissionsMask([
 			'storage' => $quotaStorage,
 			'mask' => $permissions